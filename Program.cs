96450a0b52fe406ab971831119ea942974eece16
unknown <markus.pekkenen@gmail.com>
2024-02-12 22:29:13 +0200

    create new app

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿//This is a comment to my code
+
+//rakendus küsib kasutajal sisestada tema nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Please enter your name:");
+//string - sõne
+string userName = Console.ReadLine();
+
+Console.WriteLine("Hello" + ", " + userName + "!");
+//string interpolation
+
+Console.WriteLine($"Priviet motherfucker, {userName} !");
